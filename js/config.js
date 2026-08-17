aab9167c4d9d5a743e23aa12e35b9693ec1109aa
rugbyguyIT <kyle@thesandovals.net>
2026-08-17 15:59:50 -0500

    Bulk loanee administration (js/config.js)

--- js/config.js
+++ js/config.js
@@ -13,4 +13,4 @@
 const APP_NAME = 'HLSR Asset Tracker';
 const APP_SHORT = 'HLSR Assets';
 const APP_ORG = 'Houston Livestock Show and Rodeo';
-const APP_VERSION = '0.4.0';
+const APP_VERSION = '0.5.0';
